d7e2ee17ec7f8e01f5e2080ec5d824bf05579830
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-05-10 01:13:23 -0300

    Feat: Option to include hotel for face-to-face tickets implemented.

--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,36 +1,43 @@
 import styled from 'styled-components';
 import TabTitle from '../../../components/Dashboard/Tab/TabTitle.js';
 import TabSectionTitle from '../../../components/Dashboard/Tab/TabSectionTitle.js';
-import ChoiceButton from '../../../components/Dashboard/Tab/ChoiceButton.js';
+import ChoiceButton from '../../../components/Dashboard/Tab/Payment/ChoiceButton.js';
 import { useState } from 'react';
+import ChoiceSection from '../../../components/Dashboard/Tab/Payment/ChoiceSection.js';
 
 export default function Payment() {
   const [chosenTicket, setChosenTicket] = useState(null);
+  const [chosenAccommodation, setChosenAccommodation] = useState(null);
   const ticketChoices = [
     { name: 'Presencial', price: 250, isRemote: false },
     { name: 'Online', price: 100, isRemote: true },
+  ];
+  const accommodationChoices = [
+    { name: 'Sem Hotel', price: 0, includesHotel: false },
+    { name: 'Com Hotel', price: 350, includesHotel: true },
   ];
 
   return (
     <StyleTab>
       <TabTitle>Ingresso e Pagamento</TabTitle>
 
-      <section className="ticketModality">
-        <TabSectionTitle>Primeiro, escolha sua modalidade de ingresso</TabSectionTitle>
-        <div className="choices">
-          {ticketChoices.map((ticketChoice) => {
-            return (
-              <ChoiceButton
-                key={ticketChoice.name}
-                description={ticketChoice.name}
-                price={ticketChoice.price}
-                onClick={() => setChosenTicket(ticketChoice)}
-                selected={chosenTicket?.name === ticketChoice.name ? true : false}
-              />
-            );
-          })}
-        </div>
-      </section>
+      <ChoiceSection
+        className="ticketModality"
+        title="Primeiro, escolha sua modalidade de ingresso"
+        choices={ticketChoices}
+        state={chosenTicket}
+        setState={setChosenTicket}
+      />
+
+      {chosenTicket?.name === 'Presencial' && (
+        <ChoiceSection
+          className="includesHotel"
+          title="Ótimo! Agora escolha sua modalidade de hospedagem"
+          choices={accommodationChoices}
+          state={chosenAccommodation}
+          setState={setChosenAccommodation}
+        />
+      )}
     </StyleTab>
   );
 }
@@ -40,15 +47,7 @@ const StyleTab = styled.div`
     margin-bottom: 36px;
   }
 
-  section > h2 {
-    margin-bottom: 16px;
-  }
-
-  .ticketModality > .choices {
-    display: flex;
-
-    button {
-      margin-right: 24px;
-    }
+  section {
+    margin-bottom: 44px;
   }
 `;
