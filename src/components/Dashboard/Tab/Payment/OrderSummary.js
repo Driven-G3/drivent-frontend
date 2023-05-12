4294b6fdf38f90c5ff6103349edc2678f5d271e7
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-05-12 18:03:32 -0300

    Feat: Book ticket button action

--- src/components/Dashboard/Tab/Payment/OrderSummary.js
+++ src/components/Dashboard/Tab/Payment/OrderSummary.js
@@ -2,13 +2,13 @@ import styled from 'styled-components';
 import TabSectionTitle from '../TabSectionTitle';
 import SimpleButton from '../SimpleButton';
 
-export default function OrderSummary({ sum }) {
+export default function OrderSummary({ sum, actionBtn }) {
   return (
     <OrderSummaryStyle>
       <TabSectionTitle>
         Fechado! O total ficou em <strong>R$ {sum}</strong>. Agora é só confirmar:
       </TabSectionTitle>
-      <SimpleButton text="RESERVAR INGRESSO" onClick={() => {}} />
+      <SimpleButton text="RESERVAR INGRESSO" onClick={actionBtn} />
     </OrderSummaryStyle>
   );
 }
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -5,11 +5,17 @@ import ChoiceSection from '../../../components/Dashboard/Tab/Payment/ChoiceSecti
 import OrderSummary from '../../../components/Dashboard/Tab/Payment/OrderSummary.js';
 import useEnrollment from '../../../hooks/api/useEnrollment.js';
 import WarningScreen from '../../../components/Dashboard/Tab/WarningScreen.js';
+import useTicketTypes from '../../../hooks/api/useTicketTypes.js';
+import { createTicket } from '../../../services/ticketApi.js';
+import useToken from '../../../hooks/useToken.js';
+import { toast } from 'react-toastify';
 
 export default function Payment() {
   const { enrollment } = useEnrollment();
+  const { ticketTypes } = useTicketTypes();
   const [chosenTicket, setChosenTicket] = useState(null);
   const [chosenAccommodation, setChosenAccommodation] = useState(null);
+  const token = useToken();
   const ticketChoices = [
     { name: 'Presencial', price: 250, isRemote: false },
     { name: 'Online', price: 100, isRemote: true },
@@ -18,6 +24,29 @@ export default function Payment() {
     { name: 'Sem Hotel', price: 0, includesHotel: false },
     { name: 'Com Hotel', price: 350, includesHotel: true },
   ];
+
+  function findTicketTypeId() {
+    const [type] = ticketTypes.filter((ticket) => {
+      return (
+        ticket.isRemote === !!chosenTicket.isRemote && ticket.includesHotel === !!chosenAccommodation?.includesHotel
+      );
+    });
+    return type.id;
+  }
+
+  async function bookTicket() {
+    try {
+      await createTicket(
+        {
+          ticketTypeId: findTicketTypeId(),
+        },
+        token
+      );
+      toast('Reserva feita com sucesso!');
+    } catch (error) {
+      toast('Não foi possível realizar sua reserva');
+    }
+  }
 
   if (!enrollment) {
     return (
@@ -52,7 +81,7 @@ export default function Payment() {
       {chosenTicket?.name === 'Online' && <OrderSummary sum={chosenTicket.price} />}
 
       {chosenTicket && chosenAccommodation && chosenTicket.name !== 'Online' && (
-        <OrderSummary sum={chosenTicket.price + chosenAccommodation.price} />
+        <OrderSummary sum={chosenTicket.price + chosenAccommodation.price} actionBtn={bookTicket} />
       )}
     </StyleTab>
   );
