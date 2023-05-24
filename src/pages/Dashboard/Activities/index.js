8f62d4e31a98f669d8588139f7708eee1c14a764
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-05-23 22:16:35 -0300

    Feat: If user has not a ticket paid, show a notice about it.

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,18 @@
+import WarningScreen from '../../../components/Dashboard/Tab/WarningScreen';
+import useUserTicket from '../../../hooks/api/useUserTicket';
+
 export default function Activities() {
+  const { userTicket } = useUserTicket();
+
+  if (userTicket?.status === 'RESERVED' || !userTicket) {
+    return (
+      <WarningScreen
+        tabTitle="Escolha de Atividades"
+        text="Você precisa ter confirmado pagamento antes
+              de fazer a escolha de atividades"
+      />
+    );
+  }
+
   return 'Atividades: Em breve!';
 }
