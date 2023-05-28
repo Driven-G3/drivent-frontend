260af71b6076029039b29a3194ceacb53d46599c
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-05-28 19:57:13 -0300

    FEat: Implemented event filter by data.

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -9,48 +9,58 @@ import TimesheetTrail from '../../../components/Dashboard/Tab/activities/Timeshe
 import useActivities from '../../../hooks/api/useActivities';
 import TabSectionTitle from '../../../components/Dashboard/Tab/TabSectionTitle';
 import TimeButton from './timeButton';
-import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 import { getEventInfo } from '../../../services/eventApi';
 import { eachDayOfInterval, format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
+import SimpleButton from '../../../components/Dashboard/Tab/SimpleButton';
+import DayButton from '../../../components/Dashboard/Tab/activities/DayButton';
 
 export default function Activities() {
-  const { userTicket } = useUserTicket();
-  const { activitiesRooms } = useActivitiesRooms();
-  const { activities } = useActivities();
- 
-  const [dates, setDates]= useState([]);
-  const [error, setError] = useState([]);
+  const { userTicket, userTicketLoading } = useUserTicket();
+  const { activitiesRooms, activitiesRoomsLoading } = useActivitiesRooms();
+  const { activities, activitiesLoading } = useActivities();
+  const [selectedDay, setSelectedDay] = useState();
 
-  async function days() {
-    try {
-      const response = await getEventInfo();
-      const { startsAt, endsAt } = response;
+  const weekdays = {
+    0: 'Domingo',
+    1: 'Segunda',
+    2: 'Terça',
+    3: 'Quarta',
+    4: 'Quinta',
+    5: 'Sexta',
+    6: 'Sábado',
+  };
 
-      const starts= new Date (startsAt);
-      const ends= new Date (endsAt);
+  function getDays() {
+    const days = {};
+    const daysArr = [];
 
-      const eventDays= eachDayOfInterval ({ start: starts, end: ends });
-      const formatEvent= eventDays.map((day) => format(day, 'EEE, dd/MM', { locale: ptBR }));
-      setDates(formatEvent);
-    } catch (error) {
-      setError(error);
-    }
+    activities.forEach((activity) => {
+      const date = new Date(activity.startsAt);
+      const stringDate = date.toLocaleDateString();
+      const day = date.getDate().toString().padStart(2, 0);
+      const month = date.getMonth().toString().padStart(2, 0);
+      const weekday = weekdays[date.getDay()];
+
+      if (days[stringDate]) return;
+
+      days[stringDate] = {
+        date,
+        stringDateBtn: `${weekday}, ${day}/${month}`,
+      };
+    });
+
+    const keysOfDays = Object.keys(days);
+    keysOfDays.sort((a, b) => b - a);
+
+    keysOfDays.forEach((key) => {
+      daysArr.push(days[key]);
+    });
+
+    return daysArr;
   }
 
-  useEffect(() => {
-    days();
-  }, []);
-
-  // if (error) {
-  //   return (
-  //     <WarningScreen 
-  //       tabTitle="Escolha de Atividades"
-  //       text="Algo deu errado, por favor, tente novamente."
-  //     />
-  //   );
-  // }
   if (userTicket?.status === 'RESERVED' || !userTicket) {
     return (
       <WarningScreen
@@ -71,16 +81,37 @@ export default function Activities() {
     );
   }
 
+  if (activitiesLoading || activitiesRoomsLoading || userTicketLoading) {
+    return <WarningScreen tabTitle="Escolha de Atividades" text="Carregando informações..." />;
+  }
+
   return (
     <StyleTab>
       <TabTitle>Escolha de atividades</TabTitle>
 
+      <div className="ChoiceDay">
+        {getDays().map((day) => {
+          const selected = day.date.toLocaleDateString() === selectedDay?.toLocaleDateString();
+          return (
+            <DayButton
+              key={day.stringDateBtn}
+              text={day.stringDateBtn}
+              onClick={() => setSelectedDay(day.date)}
+              selected={selected}
+            />
+          );
+        })}
+      </div>
+
       <Timesheet>
         {activitiesRooms?.map((activityRoom) => {
+          if (!selectedDay) return;
           return (
             <TimesheetTrail name={activityRoom.name}>
               {activities?.map((activity) => {
-                if (activity.locatedAtId === activityRoom.id) {
+                const activityDate = new Date(activity.startsAt);
+                const sameDate = activityDate.toDateString() === selectedDay?.toDateString();
+                if (activity.locatedAtId === activityRoom.id && sameDate) {
                   const { title, startsAt, endsAt, capacity } = activity;
                   return (
                     <EventCard
@@ -108,6 +139,7 @@ const StyleTab = styled.div`
 
 const Timesheet = styled.div`
   display: flex;
+  margin-top: 60px;
   width: 100%;
   overflow-x: scroll;
 `;
